fce6a066223dfcfc2173c7e29ac621cfcaa2d348
erayliu <rmb5241@gmail.com>
2016-12-26 16:08:15 +0800

    modify entry.jsx

--- webpack/js/entry.jsx
+++ webpack/js/entry.jsx
@@ -21,10 +21,13 @@ var myDivElement = <div className="foo">test  aaad</div>;
 var People = require('./es6.js');
 var p1 = new People("xiaolong");
 p1.sayHi();
+p1.prop = 12334;
+console.log(p1.prop);
+console.log(People.sayEle());
 
 // var ComponentMount = require('./com_mount.jsx');
-// var RepoList = require('./githubres.jsx');
+var RepoList = require('./githubres.jsx');
 
-// ReactDOM.render(<RepoList promise={$.getJSON('https://api.github.com/search/repositories?q=javascript&sort=stars')} />,
-// document.getElementById('app'));
-ReactDOM.render(myDivElement, document.getElementById('app'));
+ReactDOM.render(<RepoList promise={$.getJSON('https://api.github.com/search/repositories?q=javascript&sort=stars')} />,
+document.getElementById('app'));
+// ReactDOM.render(myDivElement, document.getElementById('app'));
